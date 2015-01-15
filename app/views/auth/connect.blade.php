b730dbe168852cb2a6c6f043ba6c2e185b149a0e
raszpi <raszpii@gmail.com>
2015-01-15 12:34:20 +0000

    connect site reworked, form class changes

--- app/views/auth/connect.blade.php
+++ app/views/auth/connect.blade.php
@@ -2,80 +2,93 @@
 
   @section('pageContent')
     <div id="content-wrapper">
+      <div class="page-header text-center">
+        <h1><i class="fa fa-link page-header-icon"></i>&nbsp;&nbsp;Connect a service</h1>
+      </div> <!-- / .page-header -->
+
       @if (!Auth::user()->isConnected())
       <div class="alert alert-warning">
         No key is provided! You are currently viewing the site in demo mode! <a href="{{ URL::route('auth.dashboard') }}" class="pull-right"> Continue in demo</a>
       </div>
       @endif
+
       <div class="col-md-10 col-md-offset-1">
-        <div class="panel panel-success panel-dark">
-          <div class="panel-heading">
-            <span class="panel-title">Connect</span>
-          </div>
-          <div class="panel-body">
 
-            <!-- PayPal connect-->
-            <div class="row">
-              <div class="col-md-4 connect-description">
-                <div class="service-icon">
-                  <a href="http://paypal.com"><i class="fa icon fa-cc-paypal"></i></a>
-                </div>
-              </div> <!-- /. connect-description -->
+        <!-- PayPal connect-->
 
-              <div class="col-md-8 connect-form">
-              <a href="{{ $redirect_url }}">
-                  <div class="col-xs-2 col-xs-offset-10 padding-xs-vr">
+        <div class="row">
+          <div class="paypal-form-wrapper">
+            <div class="panel-body paypal-form">
+              <h4>Connect PayPal</h4>
+
+              <div class="col-md-2 text-center">
+                <i class="fa icon fa-cc-paypal fa-4x"></i>
+              </div> <!-- /. col-md-2 -->
+
+              <div class="col-md-8">
+                <p class="text-muted">Some help text abot what to do. Lorem ipsum</p>
+              </div> <!-- /. col-md-8 -->
+
+              <div class="col-md-2">
+                <a href="{{ $redirect_url }}">
                     {{ Form::submit('Connect', array(
                         'id' => 'id_submit',
-                        'class' => 'btn btn-success btn-lg btn-flat pull-right')) }}
-                  </div>
+                        'class' => 'btn btn-success btn-lg btn-flat pull-right')) }}          
                 </a>
-              </div> <!-- /. connect-form -->
-            </div> <!-- /. row -->
-            <!-- /PayPal connect-->
+              </div> <!-- /. col-md-2 -->
 
-            <!-- stripe connect -->
-            <div class="row">
-              <div class="col-md-4 connect-description">
-                <div class="service-icon">
-                  <a href="http://stripe.com"><i class="fa icon fa-cc-stripe"></i></a>
-                </div>
-              </div> <!-- /. connect-description -->
+            </div> <!-- /. panel-body -->
+          </div> <!-- /. col-md-6 paypal-form-wrapper -->
+        </div> <!-- /. row -->
 
-              <div class="col-md-8 connect-form">
+        <!-- /PayPal connect-->
+
+        <!-- Stripe connect -->
+        <div class="row">
+          <div class="stripe-form-wrapper">          
+            <div class="panel-body stripe-form">
+              <h4>Connect Stripe</h4>
+
+              <div class="col-md-2 text-center">
+                <i class="fa icon fa-cc-stripe fa-4x"></i>
+              </div> <!-- /. connect-icon -->
+
+              <div class="col-md-10">
                 {{ Form::open(array(
                   'route'=>'auth.connect',
                   'method' => 'post',
                   'id' => 'form-settings',
-                  'class' => 'horizontal-form',
-                  'role' => 'form',
-                  'class' => 'panel-padding settings-form' )) }}
+                  'class' => 'form-horizontal',
+                  'role' => 'form' )) }}
 
                     <div class="form-group">
-                      {{ Form::label('id_stripe', 'Stripe key:', array(
-                        'class' => 'col-xs-4 control-label')) }}
-                      <div class="col-xs-8">
+                      {{ Form::label('id_stripe', 'Your Stripe key:', array(
+                        'class' => 'col-xs-3 control-label text-left-always')) }}
+                      <div class="col-xs-7">
                         {{ Form::text('stripe', '', array(
                           'id' => 'id_stripe',
                           'class' => 'form-control')) }}
                       </div>
-                    </div> <!-- / .form-group -->
 
-                    <div class="col-xs-2 col-xs-offset-10 padding-xs-vr">
+                      <div class="col-xs-2">
                       {{ Form::submit('Connect', array(
                           'id' => 'id_submit',
                           'class' => 'btn btn-success btn-lg btn-flat pull-right')) }}
                     </div>
+                    </div> <!-- / .form-group -->
+
+                    
 
                 {{ Form::close() }}
               </div> <!-- /. connect-form -->
-            </div> <!-- /. row -->
-            <!-- / stripe connect -->
+            </div> <!-- /. panel-body -->
+          </div> <!-- /. col-md-6 stripe-form-wrapper -->
+        </div> <!-- /. row -->
+        <!-- / Stripe connect -->
 
-          </div> <!-- /. panel-body -->
-        </div> <!-- /. panel -->
       </div> <!-- /. col-md-10 -->
     </div> <!-- / #content-wrapper -->
+
   @stop
 
   @section('pageScripts')
