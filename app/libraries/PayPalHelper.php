1f36c6516b86510b3ea6b076b4acabcfafd1b0f3
andras_buday <rise.hun@gmail.com>
2015-01-15 13:17:42 +0000

    activated more agreemetns

--- app/libraries/PayPalHelper.php
+++ app/libraries/PayPalHelper.php
@@ -64,9 +64,8 @@ class PayPalHelper {
             $token_info = $token_info->createFromRefreshToken(array('refresh_token' => $refresh_token), $api_context);
             
         } catch (Exception $ex) {
-            Log::info($ex);
             // something went wrong
-            // redirect to 500
+            echo '<pre>';print_r(json_decode($ex->getData()));
             exit(1);
         }
 
