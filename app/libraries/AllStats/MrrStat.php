78c4625cf02d7bc29350a760c92ff0fb2e2bdfc2
andras_buday <rise.hun@gmail.com>
2015-02-09 12:16:27 +0000

    MrrStat not found bug fixed

--- app/libraries/AllStats/MrrStat.php
+++ app/libraries/AllStats/MrrStat.php
@@ -5,8 +5,8 @@ class MrrStat extends BaseStat {
     /**
     * Prepare MRR for statistics
     *
-    * @param boolean 
-    * 
+    * @param boolean
+    *
     * @return array
     */
 
@@ -15,19 +15,19 @@ class MrrStat extends BaseStat {
         // defaults
         self::$statName = 'Monthly Recurring Revenue';
         self::$statID = 'mrr';
-        
-    	
+
+
     	$mrrData = array();
 
     	$mrrData = self::showSimpleStat();
 
         // full MRR data
     	if ($fullDataNeeded){
-    		
+
     		$mrrData = self::showFullStat();
 
             // data for single stat table
-			// get all the plan details 
+			// get all the plan details
 			$mrrData['detailData'] = Counter::getSubscriptionDetails();
 
 			//converting price and mrr to money format
