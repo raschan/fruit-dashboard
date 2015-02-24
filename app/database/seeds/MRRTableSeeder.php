fc35fb1cbd51485c8db8939d4a305e2596de269c
raszpi <raszpii@gmail.com>
2015-02-24 13:46:01 +0000

    database seeder implemented for mrr

--- app/database/seeds/MRRTableSeeder.php
+++ app/database/seeds/MRRTableSeeder.php
@@ -6,198 +6,30 @@ class MrrTableSeeder extends Seeder
     public function run()
     {
         DB::table('mrr')->delete();
-        DB::table('mrr')->insert(
-            array(
-                'value' => 10323,
-                'user'  => 1,
-                'date'  => '2013-01-01',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 10343,
-                'user'  => 1,
-                'date'  => '2015-01-01',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 11032,
-                'user'  => 1,
-                'date'  => '2015-01-02',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 12345,
-                'user'  => 1,
-                'date'  => '2015-01-03',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 11234,
-                'user'  => 1,
-                'date'  => '2015-01-04',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 11000,
-                'user'  => 1,
-                'date'  => '2015-01-05',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 12643,
-                'user'  => 1,
-                'date'  => '2015-01-06',
-                'provider' => 'paypal'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 14540,
-                'user'  => 1,
-                'date'  => '2015-01-07',
-                'provider' => 'paypal'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 14233,
-                'user'  => 1,
-                'date'  => '2015-01-08',
-                'provider' => 'paypal'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 15324,
-                'user'  => 1,
-                'date'  => '2015-01-09',
-                'provider' => 'paypal'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 15123,
-                'user'  => 1,
-                'date'  => '2015-01-10',
-                'provider' => 'paypal'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 15123,
-                'user'  => 1,
-                'date'  => '2015-01-11',
-                'provider' => 'paypal'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 15534,
-                'user'  => 1,
-                'date'  => '2015-01-12',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 15234,
-                'user'  => 1,
-                'date'  => '2015-01-13',
-                'provider' => 'paypal'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 16843,
-                'user'  => 1,
-                'date'  => '2015-01-14',
-                'provider' => 'paypal'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 16545,
-                'user'  => 1,
-                'date'  => '2015-01-15',
-                'provider' => 'paypal'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 17435,
-                'user'  => 1,
-                'date'  => '2015-01-16',
-                'provider' => 'paypal'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 16994,
-                'user'  => 1,
-                'date'  => '2015-01-17',
-                'provider' => 'paypal'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 18432,
-                'user'  => 1,
-                'date'  => '2015-01-18',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 19324,
-                'user'  => 1,
-                'date'  => '2015-01-19',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 20435,
-                'user'  => 1,
-                'date'  => '2015-01-20',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 21435,
-                'user'  => 1,
-                'date'  => '2015-01-21',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 21435,
-                'user'  => 1,
-                'date'  => '2015-02-02',
-                'provider' => 'stripe'
-            )
-        );
-        DB::table('mrr')->insert(
-            array(
-                'value' => 21435,
-                'user'  => 1,
-                'date'  => '2015-02-02',
-                'provider' => 'paypal'
-            )
-        );
+
+        $currentDay = time();
+        $currentDay = $currentDay - 395*24*60*60;
+        $previousValue = 13456;
+
+        for ($i = 0; $i < 415; $i++) { 
+            
+            $date = date('Y-m-d', $currentDay + $i*24*60*60);
+
+            $value = $previousValue * ((100 - rand(-15,10)) / 100);
+            var_dump($value);
+            $value = $value < 0 ? 0 : $value;
+            $previousValue = $value;
+
+            DB::table('mrr')->insert(
+                array(
+                    'value' => $value,
+                    'user' => 1,
+                    'date' => $date,
+                    'provider' => 'stripe'
+                )
+            );
+        }
+
 
         //gyt data 
         DB::table('mrr')->insert(
