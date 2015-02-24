e0caa4d9f6b771f1928e02edde5becac4d2adf58
Marcell Huszti <rashan86+bitbucket@gmail.com>
2015-02-24 16:15:17 +0100

    metrics table created

---
+++ app/models/Metric.php
@@ -0,0 +1,18 @@
+<?php
+
+class Metric extends Eloquent
+{
+	public function calculateMetrics($user) {
+
+		// calculate all the metrics
+			// monthly recurring revenue
+			// active users
+			// annual recurring revenue
+			// average recurring revenue per active user
+			// cancellations
+			// cumulative cancellations
+			// user chuen
+	}
+}
+
+?>
