193ba747b0dc150b87180ccc53cc30e7747cc767
Marcell Huszti <rashan86+bitbucket@gmail.com>
2015-03-25 14:32:34 +0100

    clean up metric definitions

--- app/libraries/Calculator.php
+++ app/libraries/Calculator.php
@@ -227,26 +227,6 @@ class Calculator
 
 
     /**
-    * MRR - Monthly Recurring Revenue
-    * all recurring revenues, per month
-    * (e.g a yearly plan is divided by 12)
-    *
-    * Required events/data:
-    *   - plan details
-    *       - subscription count
-    *       - plan cost
-    *
-    * @return int (cents)
-    */
-
-    /**
-    * Format events for dashboard transactions live feed
-    *
-    * @return array
-    */
-
-
-    /**
     * OR - Other Revenues
     * every revenue, which not connected to recurring revenues
     *
@@ -269,65 +249,6 @@ class Calculator
 
 
     /**
-    * AU - Active Users
-    * every user, who has atleast one live subscription
-    *
-    * Required events/data:
-    *   - user/customer details
-    *       - subscription data
-    *
-    * @return pos. int (heads)
-    */
-
-    public static function getActiveCustomers($user)
-    {
-        // get all the customers
-        $allCustomers = TailoredData::getCustomers($user);
-
-        // return int
-        $activeCustomers = 0;
-
-        // count all, that have at least one subscription
-        foreach ($allCustomers as $customer) {
-            if ($customer['subscriptions']['total_count'] > 0){
-                $activeCustomers++;
-            }
-        }
-
-        return $activeCustomers;
-    }
-
-    /**
-    * Save the daily Active Users number in database
-    */
-
-    public static function saveAU($user)
-    {
-        $currentDay = date('Y-m-d', time());
-
-        // checking if we already have data
-        $currentDayAU = DB::table('au')
-            ->where('date', $currentDay)
-            ->where('user', $user->id)
-            ->get();
-
-        if (!$currentDayAU) {
-            // no previous data
-            $AUValue = self::getActiveCustomers($user);
-
-            DB::table('AU')->insert(
-                array(
-                    'value' => $AUValue,
-                    'user'  => $user->id,
-                    'date'  => $currentDay
-                )
-            );
-        }
-    }
-
-
-
-    /**
     * Fees
     * all payed fees
     *
@@ -337,44 +258,6 @@ class Calculator
     * @return int (cents)
     */
 
-
-    /**
-    * Cancellations
-    * count of cancelled subscriptions
-    *
-    * Required events/data:
-    *   - cancellation events
-    *
-    * Must save also:
-    *   - the price difference
-    *
-    * @return int (pieces)
-    */
-
-    public static function saveCancellations($user) 
-    {
-        $currentDay = date('Y-m-d',time());
-
-        $currentDayCancellations = DB::table('cancellations')
-            ->where('date',$currentDay)
-            ->where('user', $user->id)
-            ->get();
-
-        if (!$currentDayCancellations)
-        {
-            $cancellationValue = self::getCancellations($user);
-
-            DB::table('cancellations')->insert(
-                array(
-                    'value' => $cancellationValue,
-                    'user'  => $user->id,
-                    'date'  => $currentDay,
-                    'provider' => 'stripe',
-                    'cumulativeValue' => CancellationStat::getIndicatorStatOnDay(time())
-                )
-            );
-        }
-    }
 
     /**
     * Downgrade
@@ -443,41 +326,6 @@ class Calculator
 
 
     /**
-    * ARR - Annual run rate
-    * MRR * 12
-    *
-    * Required functions:
-    *   - MRR
-    *
-    * @return int (cents)
-    */
-
-
-    /**
-    * ARPU - Average Revenue Per active Users
-    * MRR / active users
-    *
-    * Required functions:
-    *   - MRR
-    *   - AU
-    *
-    * @return int (cents)
-    */
-
-
-    /**
-    * UC - User Churn
-    * Cancellations / (Last month Active Users) * 100
-    *
-    * Required functions:
-    *   - Cancellations
-    *   - AU 30 days before
-    *
-    * @return int (percent)
-    */
-    
-
-    /**
     * LV - Lifetime Value
     * the average 'usefullness' of users
     * (Average Revenue Per User) / (User Churn)
@@ -509,125 +357,13 @@ class Calculator
     |------------------------------------------------------------
     */
 
-    /**
-    * Get Active User details
-    *
-    *
-    * @return array
-    */
-
-    private static function getAUDetails($user)
-    {
-        $day = date('Y-m-d', $timestamp);
-
-        $au = DB::table('au')
-            ->where('date',$day)
-            ->where('user', $user->id)
-            ->get();
-
-        if($au){
-            return $au[0]->value;
-        } else {
-            return null;
-        }
-    }
 
     /**
-    * Get Active Users on given day
+    * Format events to be displayed on dashboard
+    * @param user object
     *
-    * @param timestamp, current day timestamp
-    *
-    * @return int (cents) or null if data not exist
+    * @return array of formatted events
     */
-
-    
-
-    /**
-     * Getting all the subscriptions.
-     *
-     * @return an array with the active subscriptions
-    */
-    private static function getCurrentSubscriptions($user)
-    {
-        // intializing out array
-        $active_subscriptions = array();
-
-        // getting the customers
-        $customers = TailoredData::getCustomers($user);
-
-        // getting the active subscriptions for a customer
-        foreach ($customers as $customer) {
-
-            // going through each subscription if any
-            if ($customer['subscriptions']['total_count'] > 0) {
-                // there are some subs
-                foreach ($customer['subscriptions']['data'] as
-                         $subscription) {
-                    // updating array
-
-                    /*
-                    plan
-                        id - string
-                    start       - timestamp, subscription start date
-                    status      - string, possible values are:
-                                    'trialing'
-                                    'active'
-                                    'past_due'
-                                    'canceled'
-                                    'unpaid'
-                    quantity    - int
-                    */
-
-                    // only count subscriptions, that are active
-                    if ($subscription['status'] == 'active')
-                    {
-                        $active_subscriptions[$subscription['id']] =
-                            array(
-                                'plan_id'  => $subscription['plan']['id']
-                            );
-                    }
-                } // foreach subscriptions
-            } // if subscriptions
-        } // foreach customer
-
-        return $active_subscriptions;
-    }
-
-    public static function getSubscriptionDetails($user)
-    {
-        // get all active subscriptions
-        $currentSubscriptions = self::getCurrentSubscriptions($user);
-
-        // get all plans
-        $plans = TailoredData::getPlans($user);
-
-        // we'll store the details here
-        $planDetails = array();
-
-
-        // getting plan details
-        foreach ($plans as $id => $plan) {
-            $planDetails[$id] = array(
-                'name' => $plan['name'],
-                'price' => $plan['amount'],
-                'currency' => $plan['currency'],
-                'interval' => $plan['interval'],
-                'count' => 0,
-                'mrr' => 0
-            );
-        }
-        // getting each plan's count and mrr contribution
-        foreach ($currentSubscriptions as $subscription) {
-            $planDetail = $planDetails[$subscription['plan_id']];
-            $planDetail['count']++;
-            $planDetail['mrr'] = $planDetail['price'] * $planDetail['count'];
-            $planDetails[$subscription['plan_id']] = $planDetail;
-        }
-
-        // returning int
-        return $planDetails;
-    }
-
 
     public static function formatEvents($user)
     {
@@ -796,46 +532,9 @@ class Calculator
             } // end if stripe event
             $i++;
         }// end foreach
-            return $eventArray;
+        return $eventArray;
     }
 
-    private static function getCancellations($user)
-    {	
-    	// return value
-    	$cancellations = 0;
-
-   		// check if we reached the day before yesterday
-    	$reachedEndOfDay = false;
-
-    	$currentDay = time();
-    	$yesterDay = $currentDay - 24*60*60;
-
-    	// get all the evens saying customer cancelled
-    	$events = Event::where('user', $user->id)
-                    ->whereIn('type', ['customer.subscription.deleted'])
-                    ->orderBy('created', 'desc')
-                    ->get();
-
-        $i = 0;
-
-        // go through all the returned events and check for dates
-        while (!$reachedEndOfDay)
-        {
-        	if (strtotime($events[$i]->created) < $yesterDay) {
-        		$reachedEndOfDay = true;
-        	} else {
-        		$cancellations++;
-        	}
-
-        	$i++;
-        	if (!isset($events[$i]))
-        	{
-        		$reachedEndOfDay = true;
-        	}
-        }
-
-    	return $cancellations;
-    }
 
     /** 
     * Stores all the metrics we currently calculate
@@ -846,18 +545,29 @@ class Calculator
     public static function currentMetrics()
     {
         return array(
-            'mrr'               => array('metricClass'  => 'MrrStat'
-                                        ,'metricName'   => 'Monthly Recurring Revenue')
-            ,'au'               => array('metricClass'  => 'AUStat'
-                                        ,'metricName'   => 'Active Users')
-            ,'arr'              => array('metricClass'  => 'ArrStat'
-                                        ,'metricName'   => 'Annual Recurring Revenue')
-            ,'arpu'             => array('metricClass'  => 'ArpuStat'
-                                        ,'metricName'   => 'Average Revenue Per User')
-            ,'cancellations'    => array('metricClass'  => 'CancellationStat'
-                                        ,'metricName'   => 'Cancellations')
-            ,'uc'               => array('metricClass'  => 'UserChurnStat'
-                                        ,'metricName'   => 'User Churn')           
+            'mrr'               => array('metricClass'          => 'MrrStat'
+                                        ,'metricName'           => 'Monthly Recurring Revenue'
+                                        ,'metricDescription'    => 'MRR is a normalized measurement of recurring revenue, most frequently measured with a constant value in each month of the subscription period.')
+            
+            ,'au'               => array('metricClass'          => 'AUStat'
+                                        ,'metricName'           => 'Active Users'
+                                        ,'metricDescription'    => 'The number of customers who have at least one subscription.')
+            
+            ,'arr'              => array('metricClass'          => 'ArrStat'
+                                        ,'metricName'           => 'Annual Recurring Revenue'
+                                        ,'metricDescription'    => 'ARR is the value of the contracted recurring revenue components of your term subscriptions normalized to a one year period.')
+            
+            ,'arpu'             => array('metricClass'          => 'ArpuStat'
+                                        ,'metricName'           => 'Average Revenue Per User'
+                                        ,'metricDescription'    => 'ARPU is defined as the total revenue divided by the number of subscribers.')
+            
+            ,'cancellations'    => array('metricClass'          => 'CancellationStat'
+                                        ,'metricName'           => 'Cancellations'
+                                        ,'metricDescription'    => 'The amount of how many subscriptions were cancelled, or not renewed.')
+            
+            ,'uc'               => array('metricClass'          => 'UserChurnStat'
+                                        ,'metricName'           => 'User Churn'
+                                        ,'metricDescription'    => 'The percentage of subscribers to a service that discontinue their subscription to that service in a month.')
         );
     }
 }
