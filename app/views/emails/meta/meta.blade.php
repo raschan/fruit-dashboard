7878b4e0e406703e6c380eab1f5cfdfc51e0d9d4
Marcell Huszti <rashan86+bitbucket@gmail.com>
2015-04-10 11:00:21 +0200

    email fixes

--- app/views/emails/meta/meta.blade.php
+++ app/views/emails/meta/meta.blade.php
@@ -4,21 +4,23 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	</head>
 	<body>
-		{{-- header --}}
-		<div class='text-center'>
-			{{ HTML::image('img/fruit_logo.png','',array('class'=>'header-image')) }}
+		<div class='email-container'>
+			{{-- header --}}
+			<div class='text-center'>
+				{{ HTML::image('img/fruit_logo.png','',array('class'=>'header-image')) }}
+			</div>
+			<div class='text-right'>
+				{{ Carbon::now()->format('l, F j, Y') }}
+			</div>
+			{{-- /header --}}
+			{{-- content --}}
+			@section('emailContent')
+			@show
+			{{-- /content --}}
+			
+			{{-- footer --}}
+			<h3 class='text-center lead'>{{ HTML::link('/dashboard','Fruit Analytics') }}</h3>
+			<p class='text-center'>Copyright © {{Carbon::now()->year}} All rights reserved</p>
+			{{-- /footer --}}
 		</div>
-		<div class='text-right'>
-			{{ Carbon::now()->format('l, F j, Y') }}
-		</div>
-		{{-- /header --}}
-		{{-- content --}}
-		@section('emailContent')
-		@show
-		{{-- /content --}}
-		
-		{{-- footer --}}
-		<h3 class='text-center lead'>{{ HTML::link('/dashboard','Fruit Analytics') }}</h3>
-		<p class='text-center'>Copyright © {{Carbon::now()->year}} All rights reserved</p>
-		{{-- /footer --}}
 	</body>
