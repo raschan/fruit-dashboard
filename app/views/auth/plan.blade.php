8e6932258e740ed413922276e137550b5e75e7c3
Marcell Huszti <rashan86+bitbucket@gmail.com>
2015-04-22 13:08:50 +0200

    authcontroller and connectcontroller separated

---
+++ app/views/auth/plan.blade.php
@@ -0,0 +1,53 @@
+@extends('meta.base-user')
+
+  @section('pageContent')
+
+  <div id="content-wrapper" class='page-pricing'>
+    <div class="page-header text-center">
+      <h1><i class="page-header-icon"></i>Plans and Pricing</h1>
+    </div> <!-- / .page-header -->
+    @parent
+    <div class='plans-panel'>
+      <div class='plans-container'>
+        <!-- Free Plan -->
+        <div class='plan-col col-md-3 col-md-offset-3'>
+          <div class='plan-header bg-light-green darken'>{{$plans[1]->name}}</div>
+          <div class='plan-pricing bg-light-green'>
+            <span class='plan-value'>Free</span>
+          </div>
+          <ul class='plan-features'>
+            <li>public metrics</li>
+            <li>good for you</li>
+            <a href='/plans/free' class='bg-light-green darken'>SUBSCRIBE NOW</a>
+          </ul>
+        </div>
+        <!-- /Free Plan -->
+
+        <!-- Basic Plan -->
+        <div class='plan-col col-md-3'>
+          <div class='plan-header bg-light-green darker'>{{$plans[0]->name}}</div>
+          <div class='plan-pricing bg-light-green darken'>
+            <span class='plan-currency'>$</span>
+            <span class='plan-value'>9</span>
+            <span class='plan-period'>/MO</span>
+          </div>
+          <ul class='plan-features'>
+            <li>private data</li>
+            <li>gooder for you</li>
+            <a href='/plans/basic' class='bg-light-green darker'>SUBSCRIBE NOW</a>
+          </ul>
+        </div>
+        <!-- /Basic Plan -->
+
+        {{--
+        <!-- Elite Plan -->
+        <!-- /Elite Plan -->
+        --}}
+      </div> <!-- /.plans-container -->
+    </div> <!-- /.plans-panel -->
+  </div> <!-- /.content-wrapper -->
+  @stop
+
+  @section('pageScripts')
+
+  @stop
