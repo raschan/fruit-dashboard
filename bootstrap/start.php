ce0ee5c4768a51b5e0ea93bd98dc27e9c5b75718
Raschan <rashan86@gmail.com>
2015-05-07 21:38:04 +0200

    fix #1

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,7 +28,7 @@ $env = $app->detectEnvironment(function()
 {
 	$hostname = gethostname();
 	if ($hostname == 'server.abfinformatika.hu') {
-		if basename(dirname(__DIR__)) == 'development-dash.tryfruit.com') {
+		if (basename(dirname(__DIR__)) == 'development-dash.tryfruit.com') {
 			return 'development';
 		} elseif (basename(dirname(__DIR__)) == 'testing-dashboard.tryfruit.com') {
 			return 'deploy';
