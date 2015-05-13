45a585c16738a9ec662636b31f0a1d63845f0524
frkandris <p.toth.andras@abfinformatika.hu>
2015-05-13 16:47:48 +0200

    try & catch email error + indenting code

--- app/libraries/JobHandlers/CalculateFirstTime.php
+++ app/libraries/JobHandlers/CalculateFirstTime.php
@@ -7,18 +7,22 @@ class CalculateFirstTime
 		// we only get the user ID, get the user from it
 		$user = User::find($data['userID']);
 
-        Calculator::calculateMetricsOnConnect($user);
+                Calculator::calculateMetricsOnConnect($user);
 
-		Log::info('Sending "ready" email for user: '.$user->email);
-        $email = Mailman::make('emails.connected')
-        	->to($user->email)
-        	->subject('Your metrics are ready!')
-        	->send();
+                $user->ready = 'connected';
+                $user->save();
 
-        $user->ready = 'connected';
-        $user->save();
+                $job->delete();
 
-        $job->delete();
+                try {
+                        Log::info('Sending "ready" email for user: '.$user->email);
+                        $email = Mailman::make('emails.connected')
+                                ->to($user->email)
+                                ->subject('Your metrics are ready!')
+                                ->send();
+                } catch (Exception $e) {
+                        Log::info('Caught exception: '.$e->getMessage());
+                }
 	}
 }
 
