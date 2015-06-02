4d9add0a497396a47391d871d871f3f891fd658d
Freisinger Ádám <p.toth.andras@abfinformatika.hu>
2015-06-02 17:06:31 +0200

    add new widget url as settings

--- app/views/meta/navbar.blade.php
+++ app/views/meta/navbar.blade.php
@@ -2,7 +2,7 @@
 	<i class="dropdown-icon fa fa-2x fa-cog" id="rightDropDown" data-toggle="dropdown" aria-expanded="true"></i>
 	<ul class="dropdown-menu pull-right" role="menu" aria-labelledby="rightDropDown">
 		<li role="presentation">
-			<a href="#">
+			<a href="{{ URL::route('auth.settings') }}">
 				<i class="dropdown-icon fa fa-plus-circle"></i>&nbsp;&nbsp;Add New Widget
 			</a>
 		</li>
