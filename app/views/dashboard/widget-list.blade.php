dd4bcca27b162f65ee427d83c9080cf684751eb6
age <arzumanjan.gevorg@gmail.com>
2015-06-10 12:04:11 +0200

    widget icons: close, resize, option

--- app/views/dashboard/widget-list.blade.php
+++ app/views/dashboard/widget-list.blade.php
@@ -1,4 +1,6 @@
 <li data-id='{{ $widget_data["widget_id"] }}' class="dashboard-widget well" data-row="{{ $widget_data['position']['row'] }}" data-col="{{ $widget_data['position']['col'] }}" data-sizex="{{ $widget_data['position']['x'] }}" data-sizey="{{ $widget_data['position']['y'] }}">
+	<span class="gs-close-widgets"></span>
+	<span class="gs-option-widgets"></span>
 	<a href="{{ URL::route('connect.deletewidget', $id) }}"><span class="fa fa-times pull-right widget-close"></span></a>
 	<ul>
     @foreach ($list as $value)
--- app/views/dashboard/widget-note.blade.php
+++ app/views/dashboard/widget-note.blade.php
@@ -1,5 +1,6 @@
 <li data-id='{{ $id }}' class="dashboard-widget well" data-row="{{ $position['row'] }}" data-col="{{ $position['col'] }}" data-sizex="{{ $position['x'] }}" data-sizey="{{ $position['y'] }}">
-
+	<span class="gs-close-widgets"></span>
+	<span class="gs-option-widgets"></span>
 	{{Form::textarea('note', $currentValue, array(
 		'id' => $id,
 		'class' => 'text-fill-note note'
