da37d4c1b8c7ce34d12d64c51ce81ff873c803b8
frkandris <p.toth.andras@abfinformatika.hu>
2015-06-12 08:55:35 +0200

    file ident

--- app/views/dashboard/widget.blade.php
+++ app/views/dashboard/widget.blade.php
@@ -6,24 +6,39 @@
 @endif
 
 @if ($widget_data['widget_type'] =='google-spreadsheet-text-cell')
-  @include('dashboard.widget-text', ['text' => $widget_data['currentValue'], 'id' => $widget_data['widget_id']])
+  @include('dashboard.widget-text', [
+    'text' => $widget_data['currentValue'], 
+    'id' => $widget_data['widget_id']
+  ])
 @endif
 
 @if ($widget_data['widget_type'] =='google-spreadsheet-text-column')
-  @include('dashboard.widget-list', ['list' => $widget_data['history'], 'id' => $widget_data['widget_id']])
+  @include('dashboard.widget-list', [
+    'list' => $widget_data['history'], 
+    'id' => $widget_data['widget_id']
+  ])
 @endif
 
 @if ($widget_data['widget_type'] =='iframe')
   @include('dashboard.widget-iframe', [
-    'iframeUrl' => json_decode($widget_data["currentValue"], true)['iframeURL'],'id' => $widget_data['widget_id']])
+    'iframeUrl' => json_decode($widget_data["currentValue"], true)['iframeURL'],
+    'id' => $widget_data['widget_id']
+  ])
 @endif
 
 @if ($widget_data['widget_type'] =='google-spreadsheet-text-column-random')
-  @include('dashboard.widget-text', ['text' => $widget_data['currentValue'], 'id' => $widget_data['widget_id']])
+  @include('dashboard.widget-text', [
+    'text' => $widget_data['currentValue'], 
+    'id' => $widget_data['widget_id']
+  ])
 @endif
 
 @if ($widget_data['widget_type'] =='quote')
-  @include('dashboard.widget-quote', ['quote' => json_decode($widget_data['currentValue'],true)['quote'], 'author' => json_decode($widget_data['currentValue'],true)['author'], 'id' => $widget_data['widget_id']])
+  @include('dashboard.widget-quote', [
+    'quote' => json_decode($widget_data['currentValue'],true)['quote'], 
+    'author' => json_decode($widget_data['currentValue'],true)['author'], 
+    'id' => $widget_data['widget_id']
+  ])
 @endif
 
 @if($widget_data['widget_type'] == 'note')
