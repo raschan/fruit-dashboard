fb71c5565bec7221b9fcab71c30caac3835eb22f
age <arzumanjan.gevorg@gmail.com>
2015-06-15 15:22:21 +0200

    the general starting size of the gridbox fit the type of widget

--- app/views/dashboard/widget-note.blade.php
+++ app/views/dashboard/widget-note.blade.php
@@ -1,4 +1,4 @@
-<li data-id='{{ $id }}' class="dashboard-widget well" data-row="{{ $position['row'] }}" data-col="{{ $position['col'] }}" data-sizex="{{ $position['x'] }}" data-sizey="{{ $position['y'] }}">
+<li data-id='{{ $id }}' class="dashboard-widget well" data-row="{{ $position['row'] }}" data-col="{{ $position['col'] }}" data-sizex="3" data-sizey="3">
 	<!--a class='link-button' href='' data-toggle="modal" data-target='#widget-settings-{{ $id }}'><span class="gs-option-widgets"></span></a-->
 	<a href="{{ URL::route('connect.deletewidget', $id) }}"><span class="gs-close-widgets"></span></a>
 	
