0e5ecfd2e20fa72c384d61372ea6bfea181420da
age90 <arzumanjan.gevorg@gmail.com>
2015-06-22 17:48:08 +0200

    add new widget page with bootstrap

--- app/views/connect/connect.blade.php
+++ app/views/connect/connect.blade.php
@@ -5,162 +5,166 @@
   @stop
 
   @section('pageContent')
-    
-    <div id="content-wrapper" class="gridster not-visible">
-
-      <ul>
-
-        <li class="dashboard-widget well text-center white-background" data-row="1" data-col="1" data-sizex="1" data-sizey="1">
-          <a href="{{ URL::route('connect.addwidget', 'clock') }}">
-            <span class="icon fa fa-clock-o fa-3x"></span>
-            <p>Clock</p>
-          </a>
-        </li>
-       
-        <li class="dashboard-widget well text-center white-background" data-row="1" data-col="2" data-sizex="1" data-sizey="1">
-            <a href="{{ URL::route('connect/new', ['provider' => 'text', 'step' => 'init']) }}">
-            <span class="icon fa fa-font fa-3x"></span>
-            <p>Text widget</p>
-          </a>
-        </li>
-
-        <li class="dashboard-widget well text-center white-background" data-row="1" data-col="3" data-sizex="1" data-sizey="1">
-            <a href="{{ URL::route('connect/new', ['provider' => 'chart', 'step' => 'init']) }}">
-            <span class="icon fa fa-bar-chart fa-3x"></span>
-            <p>Chart widget</p>
-          </a>
-        </li>
-
-        <li class="dashboard-widget well text-center white-background" data-row="1" data-col="4" data-sizex="1" data-sizey="1">
-            <a href="{{ URL::route('connect/new', ['provider' => 'list', 'step' => 'init']) }}">
-            <span class="icon fa fa-list fa-3x"></span>
-            <p>List widget</p>
-          </a>
-        </li>
-
-        <li class="dashboard-widget well text-center white-background" data-row="2" data-col="5" data-sizex="1" data-sizey="1">
-            <a href="{{ URL::route('connect.addwidget', 'iframe') }}">
-            <span class="icon fa fa-file-text-o fa-3x"></span>
-            <p>iframe</p>
-          </a>
-        </li>
-
-
-        <li class="dashboard-widget well text-center white-background" data-row="2" data-col="1" data-sizex="1" data-sizey="1">
-            <a href="{{ URL::route('connect.addwidget', 'quote') }}">
-            <span class="icon fa fa-quote-left fa-3x"></span>
-            <p>Quotes</p>
-          </a>
-        </li>
-
-
-        <li class="dashboard-widget well text-center white-background" data-row="2" data-col="2" data-sizex="1" data-sizey="1">
-            <a href="{{ URL::route('connect.addwidget', 'note') }}">
-            <span class="icon fa fa-pencil fa-3x"></span>
-            <p>Notes</p>
-          </a>
-        </li>
-
-
-        <li class="dashboard-widget well text-center white-background" data-row="2" data-col="3" data-sizex="1" data-sizey="1">
-            <a href="{{ URL::route('connect.addwidget', 'greeting') }}">
-            <span class="icon fa fa-comment-o fa-3x"></span>
-            <p>Greetings</p>
-          </a>
-        </li>
-
-
-        <li class="dashboard-widget well text-center white-background" data-row="3" data-col="4" data-sizex="1" data-sizey="1">
-            <a href="{{ URL::route('connect.editwidget', 'background') }}">
-            <span class="icon fa fa-picture-o fa-3x"></span>
-            <p>Background</p>
-          </a>
-        </li>
-
-        <li class="dashboard-widget well text-center white-background" data-row="4" data-col="1" data-sizex="1" data-sizey="1">
-            <a href="{{ URL::route('connect/new', ['provider' => 'googlespreadsheet', 'step' => 'init']) }}">
-            <span class="icon fa fa-google fa-3x"></span>
-            <p>Google Spreadsheet</p>
-          </a>
-        </li>
-
-        <li class="dashboard-widget well text-center white-background" data-row="4" data-col="2" data-sizex="1" data-sizey="1">
-            <a href="{{ URL::route('connect.addwidget', 'api') }}">
-            <span class="icon fa fa-code fa-3x"></span>
-            <p>Webhook / API</p>
-          </a>
-        </li>
-
-        {{--
-      
-        <li class="dashboard-widget well text-center white-background" data-row="1" data-col="2" data-sizex="1" data-sizey="1">
-          <a href="{{ $stripeButtonUrl }}">
-            <span class="icon pf-big pf-stripe"></span>
-            <p>Stripe payments</p>
-          </a>
-        </li>
-
-        <li class="dashboard-widget well text-center white-background" data-row="1" data-col="3" data-sizex="1" data-sizey="1">
-          <a href="#" data-toggle='modal' data-target='#modal-braintree-connect'>
-            <span class="icon pf-big pf-braintree"></span>
-            <p>Braintree payments</p>
-          </a>
-        </li>
-
-        --}}
-
-      </ul>
-
-      {{--
-      <!-- Braintree details modal box -->
-      <div id='modal-braintree-connect' class='modal fade in' tabindex='-1' role='dialog' style="display:none;" aria-hidden='true'>
-        <div class='modal-dialog modal-lg'>
-          <div>
-            <div class='modal-header'>
-              <button type="button" class="close" data-dismiss='modal' aria-hidden='true'>x</button>
-              <h4 class='modal-title'>Connect Braintree</h4>
+    <div id="content-wrapper">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-2 col-sm-3 col-xs-4 no-padding">
+              <div class="settingsWidget white-background">
+              <a href="{{ URL::route('connect.addwidget', 'clock') }}">
+              <div class="settingsWidgetContent">
+                <span class="icon fa fa-clock-o fa-3x"></span>
+                <p>Clock</p>
+              </div>          <!-- content -->
+              </a>
             </div>
-            <div class='modal-content' style='background:white;'>
-              @include('connect.connect-braintree',array('user'=>$user,'stepNumber'=>$braintree_connect_stepNumber))
+            </div>          <!-- col-xs-2 -->
+            <div class="col-md-2 col-sm-3 col-xs-4 no-padding">
+              <div class="settingsWidget white-background">
+              <a href="{{ URL::route('connect/new', ['provider' => 'text', 'step' => 'init']) }}">
+              <div class="settingsWidgetContent">
+                <span class="icon fa fa-font fa-3x"></span>
+                <p>Text widget</p>
+              </div>          <!-- content -->
+              </a>
             </div>
+            </div>          <!-- col-xs-2 -->
+            <div class="col-md-2 col-sm-3 col-xs-4 no-padding">
+              <div class="settingsWidget white-background">
+              <a href="{{ URL::route('connect/new', ['provider' => 'chart', 'step' => 'init']) }}">
+              <div class="settingsWidgetContent">
+                <span class="icon fa fa-bar-chart fa-3x"></span>
+                <p>Chart widget</p>
+              </div>          <!-- content -->
+              </a>
+            </div>
+            </div>          <!-- col-xs-2 -->
+            <div class="col-md-2 col-sm-3 col-xs-4 no-padding">
+              <div class="settingsWidget white-background">
+              <a href="{{ URL::route('connect/new', ['provider' => 'list', 'step' => 'init']) }}">
+              <div class="settingsWidgetContent">
+                <span class="icon fa fa-list fa-3x"></span>
+                <p>List widget</p>
+              </div>          <!-- content -->
+              </a>
+            </div>
+            </div>          <!-- col-xs-2 -->
+            <div class="col-md-2 col-sm-3 col-xs-4 no-padding">
+              <div class="settingsWidget white-background">
+              <a href="{{ URL::route('connect.addwidget', 'iframe') }}">
+              <div class="settingsWidgetContent">
+                <span class="icon fa fa-file-text-o fa-3x"></span>
+                <p>iframe</p>
+              </div>          <!-- content -->
+              </a>
+            </div>
+            </div>          <!-- col-xs-2 -->
+            <div class="col-md-2 col-sm-3 col-xs-4 no-padding">
+              <div class="settingsWidget white-background">
+              <a href="{{ URL::route('connect.addwidget', 'quote') }}">
+              <div class="settingsWidgetContent">
+                <span class="icon fa fa-quote-left fa-3x"></span>
+                <p>Quotes</p>
+              </div>          <!-- content -->
+              </a>
+            </div>
+            </div>          <!-- col-xs-2 -->            
+            <div class="col-md-2 col-sm-3 col-xs-4 no-padding">
+              <div class="settingsWidget white-background">
+              <a href="{{ URL::route('connect.addwidget', 'note') }}">
+              <div class="settingsWidgetContent">
+                <span class="icon fa fa-pencil fa-3x"></span>
+                <p>Notes</p>
+              </div>          <!-- content -->
+              </a>
+            </div>
+            </div>          <!-- col-xs-2 -->
+            <div class="col-md-2 col-sm-3 col-xs-4 no-padding">
+              <div class="settingsWidget white-background">
+              <a href="{{ URL::route('connect.addwidget', 'greeting') }}">
+              <div class="settingsWidgetContent">
+                <span class="icon fa fa-comment-o fa-3x"></span>
+                <p>Greetings</p>
+              </div>          <!-- content -->
+              </a>
+            </div>
+            </div>          <!-- col-xs-2 -->
+            <div class="col-md-2 col-sm-3 col-xs-4 no-padding">
+              <div class="settingsWidget white-background">
+              <a href="{{ URL::route('connect.editwidget', 'background') }}">
+              <div class="settingsWidgetContent">
+                <span class="icon fa fa-picture-o fa-3x"></span>
+                <p>Background</p>
+              </div>          <!-- content -->
+              </a>
+            </div>
+            </div>          <!-- col-xs-2 -->
+            <div class="col-md-2 col-sm-3 col-xs-4 no-padding">
+              <div class="settingsWidget white-background">
+              <a href="{{ URL::route('connect/new', ['provider' => 'googlespreadsheet', 'step' => 'init']) }}">
+              <div class="settingsWidgetContent">
+                <span class="icon fa fa-google fa-3x"></span>
+                <p>Google Spreadsheet</p>
+              </div>          <!-- content -->
+              </a>
+            </div>
+            </div>          <!-- col-xs-2 -->
+            <div class="col-md-2 col-sm-3 col-xs-4 no-padding">
+              <div class="settingsWidget white-background">
+              <a href="{{ URL::route('connect.addwidget', 'api') }}">
+              <div class="settingsWidgetContent">
+                <span class="icon fa fa-code fa-3x"></span>
+                <p>Webhook / API</p>
+              </div>          <!-- content -->
+              </a>
+            </div>
+            </div>          <!-- col-xs-2 -->
+
+            {{-- 
+
+            <div class="col-xs-2 col-xs-offset-1" style="padding-left: 0; padding-right: 0; opacity: 0.75; height:125px; background-color:white; margin-right:5px; margin-bottom:5px;">
+              <a href="{{ $stripeButtonUrl }}">
+              <div class="content" style="text-align:center; padding:30px; width:100%; height:100%;">
+                <span class="icon pf-big pf-stripe"></span>
+                <p>Stripe payments</p>
+              </div>          <!-- content -->
+              </a>
+            </div>          <!-- col-xs-2 -->
+
+            <div class="col-xs-2 col-xs-offset-1" style="padding-left: 0; padding-right: 0; opacity: 0.75; height:125px; background-color:white; margin-right:5px; margin-bottom:5px;">
+              <a href="#" data-toggle='modal' data-target='#modal-braintree-connect'>
+              <div class="content" style="text-align:center; padding:30px; width:100%; height:100%;">
+                <span class="icon pf-big pf-braintree"></span>
+                <p>Braintree payments</p>
+              </div>          <!-- content -->
+              </a>
+            </div>          <!-- col-xs-2 -->
+
+            --}}
+
+            {{--
+            
+            <!-- Braintree details modal box -->
+            <div id='modal-braintree-connect' class='modal fade in' tabindex='-1' role='dialog' style="display:none;" aria-hidden='true'>
+              <div class='modal-dialog modal-lg'>
+                <div>
+                  <div class='modal-header'>
+                    <button type="button" class="close" data-dismiss='modal' aria-hidden='true'>x</button>
+                    <h4 class='modal-title'>Connect Braintree</h4>
+                  </div>
+                  <div class='modal-content' style='background:white;'>
+                    @include('connect.connect-braintree',array('user'=>$user,'stepNumber'=>$braintree_connect_stepNumber))
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /Braintree details modal box -->
+            --}}
           </div>
-        </div>
-      </div>
-      <!-- /Braintree details modal box -->
-      --}}
-      
-    </div> <!-- / #content-wrapper -->
+        </div>          <!-- col-md-10 -->
+    </div>          <!-- content-wrapper -->
 
   @stop
 
   @section('pageScripts')
-
-    <script type="text/javascript">
-      $(document).ready(function() {
-        var gridster;
-        var widget_width = $(window).width()/6-15;
-        var widget_height = $(window).height()/6-20;
-
-        $(function(){
-
-          gridster = $(".gridster ul").gridster({
-            widget_base_dimensions: [widget_width, widget_height],
-            widget_margins: [5, 5],
-            helper: 'clone',
-            resize: {
-              enabled: false,
-              max_size: [1, 1],
-              min_size: [1, 1]
-            }
-          }).data('gridster');
-
-        });
-
-        $('#content-wrapper').fadeIn(500)
-      });
-    </script>
-
-
     <!-- modal stuff for braintree start -->  
 
     @if (Session::has('modal'))
